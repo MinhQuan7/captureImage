96a0791af677368fa601c965d2c3ebf8afd0df36
MinhQuan7 <leminhquanlqd711@gmail.com>
2024-08-20 15:17:25 +0700

    change https

--- proxyServer/testProxyGet/testProxyGet.ino
+++ proxyServer/testProxyGet/testProxyGet.ino
@@ -27,15 +27,26 @@ void setup() {
 }
 
 void handleCaptureImage() {
+  Serial.println("Đã nhận được yêu cầu chụp ảnh!");
+
   HTTPClient http;
-  
-  String url = "http://admin:Eoh54321@14.241.233.207:28001/ISAPI/Streaming/channels/1/picture";
-  
+
+  // URL của camera HKVISION
+  String url = "http://14.241.233.207:28001/ISAPI/Streaming/channels/1/picture";
+
+  // Chuỗi xác thực Base64
+  String authHeader = "Basic YWRtaW46RW9oNTQzMjE="; // "admin:Eoh54321" mã hóa thành Base64
+
   http.begin(url);
-  
+  http.addHeader("Authorization", authHeader);  // Thêm Header xác thực
+
   int httpCode = http.GET();
 
+  Serial.print("HTTP Code từ HIKVISION: ");
+  Serial.println(httpCode);
+
   if (httpCode == HTTP_CODE_OK) {
+    Serial.println("Chụp ảnh thành công, đang gửi ảnh...");
     WiFiClient *stream = http.getStreamPtr();
     server.sendHeader("Content-Type", "image/jpeg");
     server.send(200, "image/jpeg", "");
@@ -45,9 +56,12 @@ void handleCaptureImage() {
       size_t len = stream->read(buffer, sizeof(buffer));
       server.client().write(buffer, len);
     }
+    Serial.println("Gửi ảnh thành công!");
   } else {
+    String payload = http.getString();
     server.send(500, "text/plain", "Lỗi khi lấy ảnh từ HIKVISION");
     Serial.printf("Lỗi HTTP từ HIKVISION: %d\n", httpCode);
+    Serial.println("Nội dung phản hồi từ HIKVISION: " + payload);
   }
 
   http.end();
